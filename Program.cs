02c9ce83a38baec381b540196d563524f2aa997b
Bulat240991 <Bulat240991@gmail.com>
2024-04-10 19:29:18 +0500

    Добавил функцию для нахождения колличества строк

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Из имеющегося массива строк формирует новый
+// массив из строк, длина которых меньше, либо равна 3 символам
+
+
+int FindCount(string[] originalArray)
+{    int count = 0;
+    // Считаем количество строк, удовлетворяющих условию
+    foreach (string str in originalArray)
+    {
+        if (str.Length <= 3)
+        {
+           count++;
+        }
+    }
+    return count;
+}
+
+string[] originalArray = { "Hello", "2", "world", ":-)" };
+int count = FindCount(originalArray);
+Console.WriteLine(count);
