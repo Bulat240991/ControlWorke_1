7ab2bc8a21ee0ee3dc6ac75b9211949eeb9aa0b3
Bulat240991 <Bulat240991@gmail.com>
2024-04-10 22:15:50 +0500

    Создал новый массив удовлетворяющий условию задачи и оформил его вывод в консоль

--- Program.cs
+++ Program.cs
@@ -2,9 +2,8 @@
 // массив из строк, длина которых меньше, либо равна 3 символам
 
 
-int FindCount(string[] originalArray)
-{    int count = 0;
-    // Считаем количество строк, удовлетворяющих условию
+int FindCount(string[] originalArray)   // Считаем количество строк, удовлетворяющих условию
+{    int count = 0;    
     foreach (string str in originalArray)
     {
         if (str.Length <= 3)
@@ -15,6 +14,30 @@ int FindCount(string[] originalArray)
     return count;
 }
 
-string[] originalArray = { "Hello", "2", "world", ":-)" };
-int count = FindCount(originalArray);
-Console.WriteLine(count);
+// Заполняем новый массив строками, удовлетворяющими условию
+string[] FindArray(string[] originalArray, int count)
+{
+    string[] newArray = new string[count];      // Создаем новый массив нужного размера
+    int index = 0;
+    foreach (string str in originalArray)
+    {
+        if (str.Length <= 3)
+        {
+            newArray[index] = str;
+            index++;
+        }
+    }
+
+        return newArray;
+}
+
+string[] originalArray = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science" };
+int count = FindCount(originalArray); 
+
+string[] newArray1 = FindArray(originalArray, count);
+
+Console.WriteLine("Новый массив строк:");       // Выводим новый массив строк
+    foreach (string str in newArray1)
+    {
+        Console.WriteLine(str);
+    }
